424274be4d31ffcbd88dc8ea4ad043f3b2af6791
Francisco Nortes Novikov <fnornov@upv.edu.es>
2024-12-10 00:04:40 +0100

    HashTable.h no me funcionaba y lo acabo de arreglar

--- HashTable.h
+++ HashTable.h
@@ -1,106 +1,88 @@
 #ifndef HASHTABLE_H
 #define HASHTABLE_H
 
-#include <iostream>
-#include <string>
 #include <ostream>
+#include <string>
 #include <stdexcept>
 #include "Dict.h"
 #include "TableEntry.h"
-
 #include "../Pr-ctica-1/ListLinked.h"
 
 template <typename V>
-class HashTable: public Dict<V> {
+class HashTable : public Dict<V> {
+	private:
+		//atributos privados
+		int n;
+		int max;
+		ListLinked<TableEntry<V> > *table;
 
-    private:
-        //atributos privados
-	int n;
-	int max;
-	ListLinked<TableEntry<V>> *table;
-	//métodos privados
-	int h(std::string key){
-		int sum = 0;
-		for (int i = 0; i < key.length(); i++) {
-			sum += int(key.at(i));
-		}
-		return sum % max;
-	}
-
-    public:
-        // métodos públicos
-	HashTable(int size){
-		max = size;
-		table = new ListLinked<TableEntry<V>>[max];
-		n = 0;
-	}
-	~HashTable(){
-		delete[] table;
-	}
-	int capacity(){
-		return max;
-	}
-	friend std::ostream& operator<<(std::ostream &out, const HashTable<V> &th) {
-		for (int i = 0; i < th.max; i++) {
-			out << i << ": ";
-            ListLinked<TableEntry<V>>& bucket = th.table[i];
-            if (bucket.size() == 0) {
-                out << "(vacía)";
-            } else {
-                for (int j = 0; j < bucket.size(); j++) {
-                    out << bucket.get(j).key << " " << bucket[j].value;
-                }
-            }
-            out << endl;
-		}
-		return out;
-	}
-	V operator[](std::string key){
-		int pos = h(key);
-		ListLinked<TableEntry<V>> aux = table[pos];
-		for(int i=0; i < table[pos].size(); i++){
-			if (aux[i].key == key) {
-				return aux[i].value;
+		//metodos privados
+		int h(std::string key){
+			int sum = 0;
+			for (int i = 0; i<key.length(); i++){
+				sum+=int(key.at(i));
 			}
+			return sum % max;
 		}
-		throw std::runtime_error("Key not found");
-	}
-	void insert(std::string key, V value) override {
-		int pos = h(key);
-		TableEntry<V> entry(key, value);
-		if (table[pos].search(entry) == -1) {
-			table[pos].append(entry);
+
+	public:
+		//metodos publicos
+		HashTable(int size){
+			n = 0;
+			max = size;
+			table = new ListLinked<TableEntry<V>> [max];
+		}
+		~HashTable(){
+			delete [] table;
+		}
+		int capacity(){
+			return max;
+		}
+		friend std::ostream& operator<<(std::ostream &out, const HashTable<V> &th){
+			for (int i = 0; i<th.max; i++){
+				if (th.table[i].empty()){
+					out << i << ": " << "empty" << std::endl;
+					continue;
+				}
+				out << i << ": " << th.table[i] << std::endl;
+			}
+			return out;
+		}
+		V operator[](std::string key){
+			return search(key);
+		}
+		void insert(std::string key, V value) override{
+			int pos = h(key);
+			TableEntry<V> entry(key, value);
+			if (table[pos].search(entry) == -1) table[pos].append(entry);
+			else throw std::runtime_error("Key already exists");
 			n++;
 		}
-	}
-	V search(std::string key) override {
-		int pos = h(key);
-		V value;
-		ListLinked<TableEntry<V>> aux = table[pos];
-		for(int i=0; i < table[pos].size(); i++){
-			if (aux[i].key == key) {
-				value = aux[i].value;
-				return value;
+		V search(std::string key) override{
+			int pos = h(key);
+			TableEntry<V> entry(key, V());
+			int i = table[pos].search(entry);
+			if (i == -1){
+				throw std::runtime_error("Key not found");
 			}
+			return table[pos].get(i).value;
 		}
-		throw std::runtime_error("Key not found");
-	}
-	V remove(std::string key) override {
-		int index = h(key);
-        ListLinked<TableEntry<V>>& bucket = table[index];
-        for (int i = 0; i < bucket.size(); i++) {
-            if (bucket.get(i).key == key) {
-                V value = bucket.get(i).value;
-                bucket.remove(i);
-                n--;
-                return value;
-            }
-        }
-        throw runtime_error("Key not found");
-	}
-	int entries() override {
-		return n;
-	}
+		V remove(std::string key) override{
+			int pos = h(key);
+			TableEntry<V> entry(key, V());
+			int i = table[pos].search(entry);
+			if (i == -1){
+				throw std::runtime_error("Key not found");
+			}
+			V value = table[pos].get(i).value;
+			table[pos].remove(i);
+			n--;
+			return value;
+		}
+		int entries() override{
+			return n;
+		}
+
 };
 
 #endif
